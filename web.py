25a61f1d924f8e24bed89b07ba2833088e2bde68
Arianna Hamilton <ariannahamilton@gmail.com>
2021-07-12 19:08:41 +0000

    user db

--- web.py
+++ web.py
@@ -1,5 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+app.config['SECRET_KEY'] = '56f662178b6ca0617379a38e5b857cee'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
 
 @app.route("/")                       # this tells you the URL the method below is related to
 @app.route("/home")  
@@ -9,6 +26,19 @@ def hello_world():
 @app.route("/about")                          # this tells you the URL the method below is related to
 def about():
     return render_template('about.html', subtitle='Home Page', subtitle2='About Page', text='This is the about page!')
+  
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
   
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
