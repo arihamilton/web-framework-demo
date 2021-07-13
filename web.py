7e51ad6f9b64b5905187170a2d01bc9426abe849
Arianna Hamilton <ariannahamilton@gmail.com>
2021-07-13 17:00:15 +0000

    add captions page for audio

--- web.py
+++ web.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+from audio import printWAV
+import time, random, threading
+from turbo_flask import Turbo
 
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
 app.config['SECRET_KEY'] = '56f662178b6ca0617379a38e5b857cee'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+interval=5
+FILE_NAME = "misery.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -39,6 +46,45 @@ def register():
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
-  
+@app.route("/captions")
+def captions():
+  TITLE = "Misery"
+  return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+
+
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
